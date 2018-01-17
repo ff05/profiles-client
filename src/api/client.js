81f7982bd3bb903cd84fd25b3d986dce1d706a2d
ff05 <frank.filius@gmail.com>
2018-01-17 13:06:02 +0100

    Add profile action creator for adding profiles

---
+++ src/api/client.js
@@ -0,0 +1,54 @@
+import request from 'superagent'
+
+export default class ApiClient {
+
+  constructor() {
+    this.host = 'http://localhost:3030'
+  }
+
+  get(path) {
+    return request
+      .get(this.createUrl(path))
+      .set(this.headers())
+  }
+
+  post(path, data = {}) {
+    return request
+      .post(this.createUrl(path))
+      .set(this.headers())
+      .send(data)
+  }
+
+  put(path, data = {}) {
+    return request
+      .put(this.createUrl(path))
+      .set(this.headers())
+      .send(data)
+  }
+
+  patch(path, data = {}) {
+    return request
+      .patch(this.createUrl(path))
+      .set(this.headers())
+      .send(data)
+  }
+
+  delete(path) {
+    return request
+      .delete(this.createUrl(path))
+      .set(this.headers())
+  }
+
+  headers() {
+    let headers = {
+      Accept: 'application/json'
+    }
+
+    return headers
+  }
+
+  // Create a full URL to our API, including the host and path
+  createUrl(path) {
+    return [this.host, path].join('')
+  }
+}
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,28 +1,33 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { Button, Form } from 'semantic-ui-react'
 import styles from './Form.css'
 
-const SuiForm = (props) => {
-  return (
-    <div className="Form">
-      <Form>
-        <Form.Field required>
-          <label>Naam</label>
-          <input placeholder='Naam' />
-        </Form.Field>
-        <Form.Field required>
-          <label>Titel</label>
-          <input placeholder='Titel' />
-        </Form.Field>
-        <Form.Field type="date" required>
-          <label>Geboortedatum</label>
-          <input placeholder='dd-mm-yyyy' />
-        </Form.Field>
-        <Form.TextArea label='Bio' placeholder='Schrijf iets over jezelf...' required />
-        <Button type='submit'>Submit</Button>
-      </Form>
-    </div>
-  )
-}
+export default class SuiForm extends PureComponent {
 
-export default SuiForm
+  handleSubmit() {
+
+  }
+  
+  render() {
+    return (
+      <div className="Form">
+        <Form>
+          <Form.Field required>
+            <label>Naam</label>
+            <input placeholder='Naam' />
+          </Form.Field>
+          <Form.Field required>
+            <label>Titel</label>
+            <input placeholder='Titel' />
+          </Form.Field>
+          <Form.Field type="date" required>
+            <label>Geboortedatum</label>
+            <input placeholder='dd-mm-yyyy' />
+          </Form.Field>
+          <Form.TextArea label='Bio' placeholder='Schrijf iets over jezelf...' required />
+          <Button type='submit'>Submit</Button>
+        </Form>
+      </div>
+    )
+  }
+}
