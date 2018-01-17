7a404001e95efce05df3addc98a93674c4fe69b7
ff05 <frank.filius@gmail.com>
2018-01-17 09:14:43 +0100

    Add Sidebar and Main to App

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Form from './Form'
+import Title from './Title'
+
+export default function Sidebar(props) {
+  return (
+    <div className="Sidebar">
+      <Title title="Nieuw Profiel" />
+    </div>
+  )
+}
