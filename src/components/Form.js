e11d4c5a91aa4eb1f75ec0c5dd89a18a1599390e
ff05 <frank.filius@gmail.com>
2018-01-18 17:06:21 +0100

    Edit App.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -31,7 +31,7 @@ class SuiForm extends PureComponent {
             <input placeholder='yyyy-mm-dd' name="birthDate" value={this.state.birthDate} onChange={this.handleChange} required />
           </Form.Field>
           <Form.TextArea label='Bio' name="bio" value={this.state.bio} placeholder='Schrijf iets over jezelf...' onChange={this.handleChange} required />
-          <Button onSubmit={this.handleSubmit} type='submit'>Submit</Button>
+          <Button className="btn-success" onSubmit={this.handleSubmit} type='submit'>Submit</Button>
         </Form>
       </div>
     )
